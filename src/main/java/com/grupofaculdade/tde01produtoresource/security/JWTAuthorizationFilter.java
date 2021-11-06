eddcf6785a8617323164fe52b1dce1601872aa61
Jhonatan Medeiros <jhonatanmedeiros7@gmail.com>
2021-11-06 12:41:05 -0300

    feat: package 'security' implemented and adjusts in imports of all project

--- src/main/java/com/grupofaculdade/tde01produtoresource/security/JWTAuthorizationFilter.java
+++ src/main/java/com/grupofaculdade/tde01produtoresource/security/JWTAuthorizationFilter.java
@@ -1,6 +1,17 @@
 package com.grupofaculdade.tde01produtoresource.security;
 
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class JWTAuthorizationFilter extends BasicAuthenticationFilter {
 
