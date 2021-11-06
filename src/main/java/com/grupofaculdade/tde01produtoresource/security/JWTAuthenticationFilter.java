572e27d89c838c4c07d6c13d1cbbb5cd3ff78484
Jhonatan Medeiros <jhonatanmedeiros7@gmail.com>
2021-11-06 11:54:07 -0300

    feat: 'JWTAuthenticationFilter' and 'UserSS' implemented

---
+++ src/main/java/com/grupofaculdade/tde01produtoresource/security/JWTAuthenticationFilter.java
@@ -0,0 +1,67 @@
+package com.grupofaculdade.tde01produtoresource.security;
+
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+
+    private AuthenticationManager authenticationManager;
+
+    private JWTUtil jwtUtil;
+
+    public JWTAuthenticationFilter(AuthenticationManager authenticationManager, JWTUtil jwtUtil) {
+        setAuthenticationFailureHandler(new JWTAuthenticationFailureHandler());
+        this.authenticationManager = authenticationManager;
+        this.jwtUtil = jwtUtil;
+    }
+
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest req,
+                                                HttpServletResponse res) throws AuthenticationException {
+
+        try {
+            CredenciaisDTO creds = new ObjectMapper()
+                    .readValue(req.getInputStream(), CredenciaisDTO.class);
+
+            UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(creds.getEmail(), creds.getSenha(), new ArrayList<>());
+
+            Authentication auth = authenticationManager.authenticate(authToken);
+            return auth;
+        }
+        catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void successfulAuthentication(HttpServletRequest req,
+                                            HttpServletResponse res,
+                                            FilterChain chain,
+                                            Authentication auth) throws IOException, ServletException {
+
+        String username = ((UserSS) auth.getPrincipal()).getUsername();
+        String token = jwtUtil.generateToken(username);
+        res.addHeader("Authorization", "Bearer " + token);
+        res.addHeader("access-control-expose-headers", "Authorization");
+    }
+
+    private class JWTAuthenticationFailureHandler implements AuthenticationFailureHandler {
+
+        @Override
+        public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception)
+                throws IOException, ServletException {
+            response.setStatus(401);
+            response.setContentType("application/json");
+            response.getWriter().append(json());
+        }
+
+        private String json() {
+            long date = new Date().getTime();
+            return "{\"timestamp\": " + date + ", "
+                    + "\"status\": 401, "
+                    + "\"error\": \"Não autorizado\", "
+                    + "\"message\": \"Email ou senha inválidos\", "
+                    + "\"path\": \"/login\"}";
+        }
+    }
+}
+}
---
+++ src/main/java/com/grupofaculdade/tde01produtoresource/security/UserSS.java
@@ -0,0 +1,53 @@
+package com.grupofaculdade.tde01produtoresource.security;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class UserSS implements UserDetails {
+
+    private Integer id;
+    private String email;
+    private String senha;
+    private Collection<? extends GrantedAuthority> authorities;
+
+    public Integer getId(){
+        return id;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return authorities;
+    }
+
+    @Override
+    public String getPassword() {
+        return senha;
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
