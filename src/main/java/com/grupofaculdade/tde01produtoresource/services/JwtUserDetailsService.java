1bbd67a3db1b3e215c33c85d2aaf00f26427e76e
Jhonatan Medeiros <jhonatanmedeiros7@gmail.com>
2021-11-11 21:24:42 -0300

    feat: 'JwtUserDetailsService' implemented

---
+++ src/main/java/com/grupofaculdade/tde01produtoresource/services/JwtUserDetailsService.java
@@ -0,0 +1,29 @@
+package com.grupofaculdade.tde01produtoresource.services;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class JwtUserDetailsService implements UserDetailsService {
+
+    @Autowired
+    private UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        com.bootstrap.springboot.model.User user = userService.getByEmail(email);
+
+        if (user.getEmail().equals(email)) {
+            return new User(email, user.getPassword(),
+                    new ArrayList<>());
+        } else {
+            throw new UsernameNotFoundException("User not found with email: " + email);
+        }
+    }
+}
