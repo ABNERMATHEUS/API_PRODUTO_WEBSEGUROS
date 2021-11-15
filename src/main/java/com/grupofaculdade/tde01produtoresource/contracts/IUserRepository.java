368b917da8fda9c22c94dc51d9f4c3c9533bf3b3
Charlie <astchars@gmail.com>
2021-11-14 22:25:06 -0300

    Adiciona user repository

---
+++ src/main/java/com/grupofaculdade/tde01produtoresource/contracts/IUserRepository.java
@@ -0,0 +1,11 @@
+package com.grupofaculdade.tde01produtoresource.contracts;
+
+import com.grupofaculdade.tde01produtoresource.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface IUserRepository extends JpaRepository<User, UUID> {
+}
